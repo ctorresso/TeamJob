e8635ae6dd1f57828488d4c7bc4db3f36a96260b
Christopher Torres <97536675+ctorresso@users.noreply.github.com>
2022-04-06 16:15:16 -0700

    Update pruebaConf.groovy

--- pruebaConf.groovy
+++ pruebaConf.groovy
@@ -1,4 +1,4 @@
-job('prueba-job-DSL') {
+job('job-DSL') {
   description('Job DSL de ejemplo para el curso de Jenkins')
   scm {
         git('https://github.com/LuisRuizLemas/Job-In-Team.git', 'main') { node ->
